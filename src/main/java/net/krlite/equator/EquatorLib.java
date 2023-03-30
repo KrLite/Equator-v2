9418914efbb39cffc36ab990e67c81c452f39a95
KrLite <68179735+KrLite@users.noreply.github.com>
2023-03-30 20:02:15 +0800

    update gradle

--- src/main/java/net/krlite/equator/EquatorLib.java
+++ src/main/java/net/krlite/equator/EquatorLib.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class EquatorLib implements ModInitializer {
-	public static final String NAME = "Equator Lib", ID = "equator";
+	public static final String NAME = "Equator", ID = "equator";
 	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
 
 	@Override
