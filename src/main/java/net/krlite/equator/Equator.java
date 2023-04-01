e5508398e051755b386c80a67ae9fea1187e8f92
KrLite <68179735+KrLite@users.noreply.github.com>
2023-04-01 13:32:59 +0800

    fix

--- src/main/java/net/krlite/equator/Equator.java
+++ src/main/java/net/krlite/equator/Equator.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class EquatorLib implements ModInitializer {
+public class Equator implements ModInitializer {
 	public static final String NAME = "Equator", ID = "equator";
 	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
 
