a103e02aadc5c38a47bd049fc1092bac6a65ca75
KrLite <68179735+KrLite@users.noreply.github.com>
2023-07-11 19:42:16 +0800

    debug

--- src/main/java/net/krlite/equator/mixin/client/TitleScreenMixin.java
+++ src/main/java/net/krlite/equator/mixin/client/TitleScreenMixin.java
@@ -1,5 +1,6 @@
 package net.krlite.equator.mixin.client;
 
+import net.krlite.equator.Equator;
 import net.krlite.equator.test.CanvasScreen;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.TitleScreen;
@@ -10,11 +11,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin {
-	/*
 	@Inject(method = "init", at = @At("HEAD"))
 	private void init(CallbackInfo info) {
-		MinecraftClient.getInstance().setScreen(new CanvasScreen());
+		if (Equator.DEBUG) MinecraftClient.getInstance().setScreen(new CanvasScreen());
 	}
-
-	// */
 }
