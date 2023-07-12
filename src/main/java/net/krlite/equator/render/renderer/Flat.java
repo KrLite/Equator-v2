1c5f708f7fbe478f39c0b8c8fc0fca019d89f6d4
KrLite <68179735+KrLite@users.noreply.github.com>
2023-07-12 21:01:15 +0800

    patch.

--- src/main/java/net/krlite/equator/render/renderer/Flat.java
+++ src/main/java/net/krlite/equator/render/renderer/Flat.java
@@ -1021,16 +1021,16 @@ public class Flat extends Basic {
 		}
 
 		public AccurateColor firstColor() {
-			return !colorMapSafe().isEmpty()
-						   ? colorMapSafe().values().stream().findFirst().orElse(Palette.TRANSPARENT)
+			return !colorMap().isEmpty()
+						   ? colorMap().values().stream().findFirst().orElse(Palette.TRANSPARENT)
 						   : existsColor()
 									 ? colorCenter()
 									 : Palette.TRANSPARENT;
 		}
 
 		public AccurateColor lastColor() {
-			return !colorMapSafe().isEmpty()
-						   ? colorMapSafe().values().stream().reduce((first, second) -> second).orElse(Palette.TRANSPARENT)
+			return !colorMap().isEmpty()
+						   ? colorMap().values().stream().reduce((first, second) -> second).orElse(Palette.TRANSPARENT)
 						   : existsColor()
 									 ? colorCenter()
 									 : Palette.TRANSPARENT;
