f59f561e9a7d311d9b2e6a81c19073cdfea44ed7
KrLite <68179735+KrLite@users.noreply.github.com>
2024-01-11 14:01:21 +0800

    OH MY GOD

--- src/main/java/net/krlite/equator/Equator.java
+++ src/main/java/net/krlite/equator/Equator.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 public class Equator implements ModInitializer {
 	public static final String NAME = "Equator", ID = "equator";
 	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
-	public static final boolean DEBUG = true;
+	public static final boolean DEBUG = false;
 
 	@Override
 	public void onInitialize() {
